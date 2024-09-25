a641bd14ecd003d4495ba89de0a262301c23d48c
Alejandro Vicinay Estrade <alvicina@c4r2s3.42urduliz.com>
2024-09-25 14:00:32 +0200

    tenemos pendiente introducir modelos, migrar, revisar archivo 0001

--- user_management_api/api/models.py
+++ user_management_api/api/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-	age = models.IntegerField()
-	name = models.CharField(max_length = 100)
+    id = models.AutoField(primary_key=True)  # SERIAL en PostgreSQL se mapea a AutoField en Django
+    email = models.EmailField(max_length=100, unique=True)  # VARCHAR(100), campo único
+    password = models.TextField()  # TEXT para la contraseña cifrada
+    username = models.CharField(max_length=50, unique=True)  # VARCHAR(50), campo único
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.username  # Esto es lo que se devolverá cuando conviertas el objeto en una cadena (por ejemplo, en el admin)
+
+    #class Meta:
+        #db_table = 'users'  # Opcional: especifica el nombre de la tabla en la base de datos
