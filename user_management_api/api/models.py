dc048f29dc7c5923bbdd05c6e0c8d0b458740bcf
Alejandro Vicinay Estrade <alvicina@c4r2s3.42urduliz.com>
2024-09-25 17:49:14 +0200

    modelo User introducido a la BBDD

--- user_management_api/api/models.py
+++ user_management_api/api/models.py
@@ -5,9 +5,30 @@ class User(models.Model):
     email = models.EmailField(max_length=100, unique=True)  # VARCHAR(100), campo único
     password = models.TextField()  # TEXT para la contraseña cifrada
     username = models.CharField(max_length=50, unique=True)  # VARCHAR(50), campo único
+    avatar = models.URLField(max_length=200, blank=True, null=True)
+    status = models.CharField(max_length=20, choices=[
+        ('online', 'Online'),
+        ('offline', 'Offline'),
+        ('in_game', 'In Game'),
+        ('busy', 'Busy')
+    ], default='offline')  # VARCHAR(20), con opciones
+    created_at = models.DateTimeField(auto_now_add=True)  # TIMESTAMP, se establece automáticamente al crear el registro
+    updated_at = models.DateTimeField(auto_now=True)  # TIMESTAMP, se actualiza automáticamente en cada modificación
+    two_factor_auth = models.BooleanField(default=False)  # BOOLEAN para la autenticación de dos factores
+    session_42 = models.TextField(blank=True, null=True)  # TEXT, para la ID de sesión de 42, puede ser nulo o estar en blanco
+
 
     def __str__(self):
         return self.username  # Esto es lo que se devolverá cuando conviertas el objeto en una cadena (por ejemplo, en el admin)
 
-    #class Meta:
-        #db_table = 'users'  # Opcional: especifica el nombre de la tabla en la base de datos
+    class Meta:
+        db_table = 'users'  # Opcional: especifica el nombre de la tabla en la base de datos
+
+    
+
+
+from api.models import User
+users = User.objects.all()
+for user in users:
+    print(user)
+
